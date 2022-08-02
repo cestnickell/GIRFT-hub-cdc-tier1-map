40b8f6f99bf165c94bc21b1f9718338c86b99d1f
cestnickell <78605632+cestnickell@users.noreply.github.com>
2022-08-02 15:59:15 +0100

    Add files via upload

---
+++ data/202207Tier1hospitalscsvexport_3.js
@@ -0,0 +1,26 @@
+var json_202207Tier1hospitalscsvexport_3 = {
+"type": "FeatureCollection",
+"name": "202207Tier1hospitalscsvexport_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Regions": "Midlands", "Provider": "University Hospitals Birmingham NHS Foundation Trust", "Postcode": "B15 2TH" }, "geometry": { "type": "Point", "coordinates": [ -1.93966997, 52.45194244 ] } },
+{ "type": "Feature", "properties": { "Regions": "Midlands", "Provider": "Worcestershire Acute Hospitals NHS Trust", "Postcode": "WR5 1DD" }, "geometry": { "type": "Point", "coordinates": [ -2.181144, 52.19120026 ] } },
+{ "type": "Feature", "properties": { "Regions": "Midlands", "Provider": "University Hospitals of Leicester NHS Trust", "Postcode": "LE1 5WW" }, "geometry": { "type": "Point", "coordinates": [ -1.1679051, 52.6267719 ] } },
+{ "type": "Feature", "properties": { "Regions": "NEY", "Provider": "Hull University Teaching Hospitals NHS Trust", "Postcode": "HU3 2JZ" }, "geometry": { "type": "Point", "coordinates": [ -0.358271, 53.74441147 ] } },
+{ "type": "Feature", "properties": { "Regions": "NEY", "Provider": "North Cumbria Integrated Care NHS Foundation Trust", "Postcode": "CA2 7HY" }, "geometry": { "type": "Point", "coordinates": [ -2.9594516, 54.8961775 ] } },
+{ "type": "Feature", "properties": { "Regions": "NW", "Provider": "East Lancashire Hospital NHS Trust", "Postcode": "BB10 2PQ" }, "geometry": { "type": "Point", "coordinates": [ -2.2292305, 53.8098503 ] } },
+{ "type": "Feature", "properties": { "Regions": "NW", "Provider": "Lancashire Teaching Hospitals NHS Foundation Trust", "Postcode": "PR2 9HT" }, "geometry": { "type": "Point", "coordinates": [ -2.70740294, 53.79135132 ] } },
+{ "type": "Feature", "properties": { "Regions": "NW", "Provider": "Liverpool University Hospitals NHS Foundation Trust", "Postcode": "L14 3PE" }, "geometry": { "type": "Point", "coordinates": [ -2.90067196, 53.40997314 ] } },
+{ "type": "Feature", "properties": { "Regions": "NW", "Provider": "Manchester University NHS Foundation Trust", "Postcode": "M13 9WL" }, "geometry": { "type": "Point", "coordinates": [ -2.2950539, 53.4250233 ] } },
+{ "type": "Feature", "properties": { "Regions": "EOE", "Provider": "Bedfordshire Hospital NHS Foundation Trust", "Postcode": "MK42 9DJ" }, "geometry": { "type": "Point", "coordinates": [ -0.47247401, 52.12827682 ] } },
+{ "type": "Feature", "properties": { "Regions": "EOE", "Provider": "Mid and South Essex NHS Foundation Trust", "Postcode": "SS0 0RY" }, "geometry": { "type": "Point", "coordinates": [ 0.6855441, 51.5538626 ] } },
+{ "type": "Feature", "properties": { "Regions": "EOE", "Provider": "Norfolk and Norwich University Hospitals NHS Foundation Trust", "Postcode": "NR4 7UY" }, "geometry": { "type": "Point", "coordinates": [ 1.22106695, 52.61808395 ] } },
+{ "type": "Feature", "properties": { "Regions": "EOE", "Provider": "East and North Hertfordshire NHS Trust", "Postcode": "SG1 4AB" }, "geometry": { "type": "Point", "coordinates": [ -0.212726, 51.92461014 ] } },
+{ "type": "Feature", "properties": { "Regions": "EOE", "Provider": "West Hertfordshire Hospitals NHS Trust", "Postcode": "WD18 0HB" }, "geometry": { "type": "Point", "coordinates": [ -0.40447199, 51.64905167 ] } },
+{ "type": "Feature", "properties": { "Regions": "SW", "Provider": "Torbay and South Devon NHS Foundation Trust", "Postcode": "TQ2 7AA" }, "geometry": { "type": "Point", "coordinates": [ -3.55379009, 50.48232651 ] } },
+{ "type": "Feature", "properties": { "Regions": "SW", "Provider": "Royal Devon and Exeter NHS Foundation Trust", "Postcode": "EX2 5DW" }, "geometry": { "type": "Point", "coordinates": [ -3.50669408, 50.71669006 ] } },
+{ "type": "Feature", "properties": { "Regions": "SW", "Provider": "University Hospitals Plymouth NHS Trust", "Postcode": "PL6 5FP" }, "geometry": { "type": "Point", "coordinates": [ -4.1182876, 50.4168004 ] } },
+{ "type": "Feature", "properties": { "Regions": "SW", "Provider": "North Bristol NHS Trust", "Postcode": "BS10 5NB" }, "geometry": { "type": "Point", "coordinates": [ -2.59294796, 51.49733353 ] } },
+{ "type": "Feature", "properties": { "Regions": "London", "Provider": "North Middlesex University Hospital NHS Trust", "Postcode": "N18 1QX" }, "geometry": { "type": "Point", "coordinates": [ -0.074341, 51.61312103 ] } }
+]
+}
