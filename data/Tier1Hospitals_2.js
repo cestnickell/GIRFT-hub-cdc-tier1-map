1bc38c0a260722b8d812785cf5acde82f902a886
cestnickell <78605632+cestnickell@users.noreply.github.com>
2022-12-08 12:07:04 +0000

    Updated locations and symbol
    Updated to reflect new CDC location list and hub list. Symbols tweaked to improve readability on overlap. Terminology of STPs updated to ICB.

---
+++ data/Tier1Hospitals_2.js
@@ -0,0 +1,26 @@
+var json_Tier1Hospitals_2 = {
+"type": "FeatureCollection",
+"name": "Tier1Hospitals_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Provider": "University Hospitals Birmingham NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -1.93966997, 52.45194244 ] } },
+{ "type": "Feature", "properties": { "Provider": "Worcestershire Acute Hospitals NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.181144, 52.19120026 ] } },
+{ "type": "Feature", "properties": { "Provider": "University Hospitals of Leicester NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -1.1679051, 52.6267719 ] } },
+{ "type": "Feature", "properties": { "Provider": "Hull University Teaching Hospitals NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -0.358271, 53.74441147 ] } },
+{ "type": "Feature", "properties": { "Provider": "North Cumbria Integrated Care NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.9594516, 54.8961775 ] } },
+{ "type": "Feature", "properties": { "Provider": "East Lancashire Hospital NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.2292305, 53.8098503 ] } },
+{ "type": "Feature", "properties": { "Provider": "Lancashire Teaching Hospitals NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.70740294, 53.79135132 ] } },
+{ "type": "Feature", "properties": { "Provider": "Liverpool University Hospitals NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.90067196, 53.40997314 ] } },
+{ "type": "Feature", "properties": { "Provider": "Manchester University NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.2950539, 53.4250233 ] } },
+{ "type": "Feature", "properties": { "Provider": "Bedfordshire Hospital NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -0.47247401, 52.12827682 ] } },
+{ "type": "Feature", "properties": { "Provider": "Mid and South Essex NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ 0.6855441, 51.5538626 ] } },
+{ "type": "Feature", "properties": { "Provider": "Norfolk and Norwich University Hospitals NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ 1.22106695, 52.61808395 ] } },
+{ "type": "Feature", "properties": { "Provider": "East and North Hertfordshire NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -0.212726, 51.92461014 ] } },
+{ "type": "Feature", "properties": { "Provider": "West Hertfordshire Hospitals NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -0.40447199, 51.64905167 ] } },
+{ "type": "Feature", "properties": { "Provider": "Torbay and South Devon NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -3.55379009, 50.48232651 ] } },
+{ "type": "Feature", "properties": { "Provider": "Royal Devon and Exeter NHS Foundation Trust" }, "geometry": { "type": "Point", "coordinates": [ -3.50669408, 50.71669006 ] } },
+{ "type": "Feature", "properties": { "Provider": "University Hospitals Plymouth NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -4.1182876, 50.4168004 ] } },
+{ "type": "Feature", "properties": { "Provider": "North Bristol NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -2.59294796, 51.49733353 ] } },
+{ "type": "Feature", "properties": { "Provider": "North Middlesex University Hospital NHS Trust" }, "geometry": { "type": "Point", "coordinates": [ -0.074341, 51.61312103 ] } }
+]
+}
